38721e0bd5fb4be24e87afd65223befd17cb8f4f
confused-Techie <dev@lhbasics.com>
2023-04-09 20:42:49 -0700

    Added error checking to `dev` and automatic port reassignment if port is in use

--- scripts/dev.js
+++ scripts/dev.js
@@ -4,10 +4,40 @@ const config = require("../site.config.js").config;
 
 const app = express();
 
+let serve;
+
 const port = config.devPort ?? 8080;
 
 app.use("/", express.static("dist"));
 
-app.listen(port, () => {
-  console.log(`Dev site running at: http://localhost:${port}/`);
+function startListener() {
+  try {
+    serve = app.listen(port, () => {
+      console.log(`Dev site running at: http://localhost:${port}/`);
+    });
+  } catch(err) {
+    if (err.code === "EADDRINUSE") {
+      port = port + 1;
+      startListener();
+    } else {
+      console.error(err);
+      process.exit(100);
+    }
+  }
+}
+
+startListener();
+
+process.on("SIGTERM", async () => {
+  console.log("SIGTERM signal received. Shutting down...");
+  serve.close(() => {
+    console.log("HTTP Server closed.");
+  });
+});
+
+process.on("SIGINT", async () => {
+  console.log("SIGINT signal received. Shutting down...");
+  serve.close(() => {
+    console.log("HTTP Server closed.");
+  });
 });
