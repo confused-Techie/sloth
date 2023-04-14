e175a1f12c6970e29f09d03e69effce157c035d8
confused-Techie <dev@lhbasics.com>
2023-04-13 19:53:12 -0700

    Revert changes

--- src/dev-server.js
+++ src/dev-server.js
@@ -4,16 +4,15 @@ const path = require("path");
 const userConfig = require(path.join(process.cwd(), "./site.config.js"));
 
 const app = express();
-let LiveReloadExpress = require("livereload-express")(app);
 
 let serve;
 
 let port = userConfig.config.devPort ?? 8080;
 
-app.use("/", LiveReloadExpress.static(path.join(process.cwd(), userConfig.config.buildDirectory)));
+app.use("/", express.static(path.join(process.cwd(), userConfig.config.buildDirectory)));
 
 function startListener() {
-  serve = LiveReloadExpress.listen(port, () => {
+  serve = app.listen(port, () => {
     console.log(`Dev site running at: http://localhost:${port}/`);
   });
 }
