1921504c170852e8ddbaf8d75e7bb95ed849815b
confused-Techie <dev@lhbasics.com>
2023-04-13 17:24:35 -0700

    Testing `livereload-express`

--- src/dev-server.js
+++ src/dev-server.js
@@ -4,15 +4,16 @@ const path = require("path");
 const userConfig = require(path.join(process.cwd(), "./site.config.js"));
 
 const app = express();
+let LiveReloadExpress = require("livereload-express")(app);
 
 let serve;
 
 let port = userConfig.config.devPort ?? 8080;
 
-app.use("/", express.static(path.join(process.cwd(), userConfig.config.buildDirectory)));
+LiveReloadExpress.use("/", express.static(path.join(process.cwd(), userConfig.config.buildDirectory)));
 
 function startListener() {
-  serve = app.listen(port, () => {
+  serve = LiveReloadExpress.listen(port, () => {
     console.log(`Dev site running at: http://localhost:${port}/`);
   });
 }
